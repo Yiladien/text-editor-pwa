3a941d96c56a84bed2068257af6c9d9a2cdd6168
yiladien@hotmail.com <yiladien@hotmail.com>
2022-11-29 04:00:50 -0600

    added service worker

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -9,6 +9,13 @@ import Logo from "../images/logo.png";
 // Stylesheets
 import "../css/style.css";
 
+if ("serviceWorker" in navigator) {
+  // Use the window load event to keep the page load performant
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("./service-worker.js");
+  });
+}
+
 const main = document.querySelector("#main");
 main.innerHTML = "";
 
